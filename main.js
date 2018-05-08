9195df1202d8da3548b6394adb04dd66f2e47a08
Adi <avrabac@gmail.com>
2018-05-08 19:28:59 +0200

    New update

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var button1=document.querySelector("#button1");
 var avion1=document.querySelector("#avion");
 var testp=document.querySelector("#test-p");
-var noimage=document.querySelector("#nopic");
+var body=document.getElementsByTagName("body");
 var color1=document.querySelector("#color1");
 var color2=document.querySelector("#color2");
-var body=document.getElementById("document");
 
 button1.addEventListener("mouseover",function(){
     //testp.style.color="orange";
@@ -21,16 +20,3 @@ button1.addEventListener("mouseout",function(){
     avion1.style.transform="rotate(0deg)";
     avion.style.opacity="0.1";
 });
-
-noimage.addEventListener("click",function(){
-    body.style.backgroundImage="none";
-    body.style.color="linear-gradient(to right , yelow, orange)";
-})
-
-function changeColor(){
-    body.style.backgroundImage="none";
-    body.style.background="linear-gradient(to right , "+ color1.value+" , "+color2.value+" )";
-
-}
-color1.addEventListener("input", changeColor);
-color2.addEventListener("input", changeColor);
