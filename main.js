2abc51805370154791650ba350e1302bb877396e
Adi <avrabac@gmail.com>
2018-05-12 22:28:45 +0200

    Update with form for becoming a member

--- main.js
+++ main.js
@@ -28,9 +28,48 @@ $(document).ready(function(){
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
-function myFunction1(){
-   var popuptext=document.getElementsByClassName(".popuptext");
-   popuptext.style.visibility="hidden";
-    
+function myFunction4(){
+  var checkBox=document.getElementById("myCheck");
+var inputBox=document.getElementById("addCourses");
+
+if(checkBox.checked===true){
+  inputBox.style.display="block";
+} else{
+  inputBox.style.display="none";
+ }
 }
-popup.addEventListener(onmouseleave, myFunction1());
+function myFunction5(){
+  var checkBox=document.getElementById("myCheck1");
+  var inputBox1=document.getElementById("addSchool");
+  
+  if(checkBox.checked===true){
+    inputBox1.style.display="block";
+  } else{
+    inputBox1.style.display="none";
+   }
+  }
+function myFunction3(){
+  var getInvolved=document.getElementById("getInvolved");
+  var courseLabel=document.getElementById("specCoursesLabel");
+  var checkBox=document.getElementById("myCheck");
+  var schoolLabel=document.getElementById("specSchoolLabel");
+  var checkBox1=document.getElementById("myCheck1");
+  var inputBox1=document.getElementById("addSchool");
+  var inputBox=document.getElementById("addCourses");
+ 
+
+  if(getInvolved.checked===true){
+    courseLabel.style.display="block";
+    checkBox.style.display="block";
+    schoolLabel.style.display="block";
+    checkBox1.style.display="block";
+  }else {
+    courseLabel.style.display="none";
+    checkBox.style.display="none";
+    schoolLabel.style.display="none";
+    checkBox1.style.display="none";
+    inputBox.style.display="none";
+    inputBox1.style.display="none";
+  }
+
+}
